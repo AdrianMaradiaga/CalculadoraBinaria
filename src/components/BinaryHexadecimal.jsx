29322be61043e4ca53d0a70f3ddd3e86df5d77da
_adrianmaradiaga <adrianmaradiaga07@gmail.com>
2023-10-21 21:09:32 -0600

    refactor: comentarios explicativos

--- src/components/BinaryHexadecimal.jsx
+++ src/components/BinaryHexadecimal.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import './BinaryHexadecimal.css'
 
 const BinaryHexadecimal = () => {
-  const [binaryInput, setBinaryInput] = useState("");
-  const [hexadecimalOutput, setHexadecimalOutput] = useState("");
+  const [binaryInput, setBinaryInput] = useState(""); // Estado para el valor binario de entrada
+  const [hexadecimalOutput, setHexadecimalOutput] = useState(""); // Estado para el valor hexadecimal de salida
 
   const handleConvert = () => {
     const binaryNumber = binaryInput;
@@ -12,17 +12,21 @@ const BinaryHexadecimal = () => {
     setHexadecimalOutput(hexadecimalNumber);
   };
 
+  // Función para convertir un número binario a hexadecimal
   const binario_a_hexadecimal = (binary) => {
+    // Primero, convertimos el número binario a decimal
     const decimalNumber = parseInt(binary, 2); // Convierte de binario a decimal
+    // Luego, convertimos el número decimal a hexadecimal
     const hexadecimalNumber = decimalNumber.toString(16); // Convierte de decimal a hexadecimal
 
-    return hexadecimalNumber;
+    return hexadecimalNumber; // Devolvemos el valor hexadecimal resultante
   };
 
   const handleInputChange = (e) => {
     const inputValue = e.target.value;
     const validBinary = /^[01]*$/;
     if (validBinary.test(inputValue)) {
+      // Actualizamos el estado del valor binario de entrada si es válido (solo contiene 0 y 1)
       setBinaryInput(inputValue);
     }
   };
